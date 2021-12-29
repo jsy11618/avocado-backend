55ebe23eae99255b14a33b8fdc0b121fd6a0fa68
gomino90 <cyzhekfek@naver.com>
2021-12-29 16:29:59 +0900

    modify gitignore

--- src/main/java/avocado/ecommercebackend/reviewList/repository/ReviewListRepository.java
+++
@@ -1,7 +0,0 @@
-package avocado.ecommercebackend.reviewList.repository;
-
-import avocado.ecommercebackend.reviewList.model.ReviewList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewListRepository extends JpaRepository<ReviewList, Long> {
-}
