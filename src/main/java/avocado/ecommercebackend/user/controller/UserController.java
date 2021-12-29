213ed2a59d7873cd992fd428f2bb61619541fa24
jsy11618 <jsy11618@gmail.com>
2021-12-29 15:58:03 +0900

    User

--- src/main/java/avocado/ecommercebackend/user/controller/UserController.java
+++ src/main/java/avocado/ecommercebackend/user/controller/UserController.java
@@ -3,23 +3,26 @@ package avocado.ecommercebackend.user.controller;
 
 import avocado.ecommercebackend.user.model.User;
 import avocado.ecommercebackend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@RequestMapping("/api")
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @PostMapping("/add")
-    public User add(@RequestBody User user){return userService.addUser(user);}
+    @PostMapping("/user/addUser")
+    public User addUser(@RequestBody User user){return userService.addUser(user);}
 
-    @GetMapping("/getAll")
+    @GetMapping("/user/getAllUser")
     public List<User> getAllUser(){return userService.getAllUser();}
 
-
+    @GetMapping("/user/getUser/{id}")
+    public Optional<User> getUser(@PathVariable Long id){return userService.getUser(id);}
 }
