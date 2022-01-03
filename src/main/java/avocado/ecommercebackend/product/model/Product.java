b7abcd05db4a5e1d7319d730d9b8b1e7debb78bd
gomino90 <cyzhekfek@naver.com>
2022-01-03 11:54:41 +0900

    up

--- src/main/java/avocado/ecommercebackend/product/model/Product.java
+++ src/main/java/avocado/ecommercebackend/product/model/Product.java
@@ -1,5 +1,6 @@
 package avocado.ecommercebackend.product.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -18,5 +19,33 @@ public class Product {
     private String productName;
     private int stockQuantity;
     private int price;
+    private int sale;
+    private int rate;
+    private String productDetail;
+    private Boolean isSlide;
+    private Boolean isTrend;
+    private Boolean isBest;
+    private Boolean isTodayDeal;
+    private Boolean isAlmostSold;
+
+
+    public Product() {
+    }
+
+    @Builder
+
+    public Product(String productName, int stockQuantity, int price, int sale, int rate, String productDetail, Boolean isSlide, Boolean isTrend, Boolean isBest, Boolean isTodayDeal, Boolean isAlmostSold) {
+        this.productName = productName;
+        this.stockQuantity = stockQuantity;
+        this.price = price;
+        this.sale = sale;
+        this.rate = rate;
+        this.productDetail = productDetail;
+        this.isSlide = isSlide;
+        this.isTrend = isTrend;
+        this.isBest = isBest;
+        this.isTodayDeal = isTodayDeal;
+        this.isAlmostSold = isAlmostSold;
+    }
 
 }
